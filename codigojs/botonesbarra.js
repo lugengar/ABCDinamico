141e9a70a9a8f50956a65aaf787df757de23a30d
lugengar <luciano.barbinii@gmail.com>
2024-07-12 02:11:56 -0300

    a

--- codigojs/botonesbarra.js
+++
@@ -1,8 +0,0 @@
-
-const barra = document.querySelectorAll('.barradebusqueda');
-function barradebusqueda(id){
-    barra.forEach(barra => barra.classList.remove('activo'));
-    document.getElementById(id).classList.add('activo');
-    document.getElementById(id).scrollIntoView({ behavior: 'smooth' });
-
-}
--- codigophp/crearmapa.php
+++
@@ -1,6 +0,0 @@
-<?php
-function crearmapa($ubicacion){
-    return 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3287.4006114986105!2d-58.53745522416194!3d-34.51807695298058!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcb0946037da75%3A0x7fae4b92e6699b59!2s'.urlencode($ubicacion).'!5e0!3m2!1ses-419!2sar!4v1684382444792!5m2!1ses-419!2sar';
-}
-
-?>
